680f814e405935b5f0865d72420e51702a7f6d1b
Mike Bujold <mike@virgintechnologies.com>
2020-04-30 16:19:51 -0600

    First commit

---
+++ thermostat.ino
@@ -0,0 +1,78 @@
+#define SDA_PORT PORTB
+#define SDA_PIN 0
+#define SCL_PORT PORTB
+#define SCL_PIN 2
+
+// config for tinyprint lib
+#define TP_PRINTLINES 1
+#define TP_FLASHSTRINGHELPER 0
+#define TP_NUMBERS 1
+#define TP_FLOAT 1
+#define TP_WINDOWSLINEENDS 0
+
+// Includes for OLED display
+#include "SH1106Lib.h"
+#include "glcdfont.h"
+
+// Includes for reading temp sensor
+#include "OneWire.h"
+
+SH1106Lib display;
+
+OneWire ow(3);
+
+void setup(){
+  pinMode(A2, INPUT);
+  
+  display.initialize();
+  display.clearDisplay();
+  display.setFont(font,5,7);
+  display.setTextWrap(true);
+  display.setTextColor(WHITE, TRANSPARENT);
+  display.clearDisplay();
+  // temp sensor
+  //while(!ow.search(addr)){
+  //  delay(750);
+  //}
+  display.setCursor(0,0);
+  display.println("Thermostat Test");
+}
+
+double readTemp(void){
+  byte data[12];
+  int16_t raw;
+  
+  ow.reset();
+  ow.skip();
+  ow.write(0x44);
+  delay(1000);
+  ow.reset();
+  ow.skip();
+  ow.write(0xBE);
+  byte i;
+  for (i=0; i<9; i++){
+    data[i] = ow.read();
+  }
+
+  raw = (data[1] << 8) | data[0];
+  return (float)raw / 16.0;
+ 
+}
+
+void loop(){
+  double temp;
+  temp = readTemp();
+  int analogVal = analogRead(A2);
+  // Draw box to erase old text
+  display.fillRect(0,8,30,20,BLACK);
+  // Write data
+  display.setCursor(0,8);
+  display.println(temp, 2);
+  display.setCursor(0,16);
+  display.println(analogVal, 10);
+
+  delay(500);
+  
+  
+}
+
